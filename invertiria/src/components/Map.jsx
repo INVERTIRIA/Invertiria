fbed44c44e9f27586996549b79b2af87a56ae717
kevinquique <kevinlopez1208@yahoo.es>
2025-03-17 16:40:00 -0500

    commit

--- invertiria/src/components/Map.jsx
+++ invertiria/src/components/Map.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 
 // Mapa
 function Map() {
 
+    const { t } = useTranslation();
+
     // Coordenadas iniciales (Colombia)
     const initialPosition = [4, -73];
 
     const [position, setPosition] = useState(null);
+    const [location, setLocation] = useState(null);
 
     // Función para actualizar las coordenadas
     const changePosition = (position) => {
         setPosition(position);
+        getLocation(position.lat, position.lng, setLocation)
     };
 
     return (
@@ -21,25 +26,28 @@ function Map() {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                
+
                 {/* Pasa la función de actualización al componente LocationMarker */}
                 <LocationMarker onPositionChange={changePosition} />
 
                 {/* Muestra un marcador si hay coordenadas */}
                 {position && (
                     <Marker position={position}>
-                        <Popup>{position.lat.toFixed(4)}, {position.lng.toFixed(4)}</Popup>
+                        <Popup>{location}</Popup>
                     </Marker>
                 )}
             </MapContainer>
 
             {/* Mostrar coordenadas */}
-            {position ? (
-                <p className="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-cyan-50">
-                    Latitud: {position.lat.toFixed(4)}, Longitud: {position.lng.toFixed(4)}
-                </p>
+            {location ? (
+                <>
+                    <p className="mx-auto mt-3 max-w-xl text-l/8 text-pretty text-cyan-50">{location}</p>
+                    {/* <p className="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-cyan-50">
+                        Latitud: {position.lat.toFixed(4)}, Longitud: {position.lng.toFixed(4)}
+                    </p> */}
+                </>
             ) : (
-                <p className="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-cyan-50">Haz clic en el mapa para seleccionar la ubicación</p>
+                <p className="mx-auto mt-3 max-w-xl text-l/8 text-pretty text-cyan-50">{t("investment.click_on_the_map")}</p>
             )}
         </div>
     );
@@ -52,4 +60,20 @@ function LocationMarker({ onPositionChange }) {
     useMapEvents({
         click(e) { onPositionChange(e.latlng) }
     });
+}
+
+// FUNCIONES
+
+// Obtener la ubicación
+async function getLocation(lat, lng, setLocation) {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=geocodejson&lat=${lat}&lon=${lng}&zoom=10&addressdetails=1`;
+    fetch(url, { headers: { 'User-Agent': 'testApp/0.1 (kevin@banderaonline.org)' } })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setLocation(data.features[0].properties.geocoding.label);
+        })
+        .catch(error => {
+            console.error(error);
+        })
 }
