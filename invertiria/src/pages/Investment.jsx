c8ffe549f3784b0cfafd037783a40e7dc8dc39f5
kevinquique <kevinlopez1208@yahoo.es>
2025-03-17 17:38:35 -0500

    commit

--- invertiria/src/pages/Investment.jsx
+++ invertiria/src/pages/Investment.jsx
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { irr } from 'financial'
 
 // Componentes
 import { InvestForm } from "../components/InvestForm";
@@ -9,18 +11,43 @@ function Investment() {
 
   const { t } = useTranslation();
 
+  const [results, setResults] = useState([]);
+
+  // Flujo de caja
+  const cashFlows = [-1000, 300, 400, 500, 200];
+
+  useEffect(() => {
+    getResults(cashFlows, setResults);
+  }, []);
+
   return (
     <>
       <InvestForm
         title={t("investment.title")}
         paragraph={t("investment.paragraph")}
       />
-      <InvestResult 
+      <InvestResult
         title={"Resultados"}
-        paragraph={"Aqui estan los resultados de tu inversion"}
+        paragraph={"Aqui estan los resultados de tu inversion:"}
+        results={
+          <>
+            <p className="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-cyan-50">Cash Flows: {cashFlows.toString()}</p>
+            <p className="mx-auto mt-2 max-w-xl text-lg/8 text-pretty text-cyan-50">TIR: {results[0]}</p>
+          </>
+        }
       />
     </>
   )
 }
 
 export { Investment }
+
+// FUNCIONES
+function getResults(cashFlows, setResults) {
+
+  // Calcular la TIR
+  const tir = irr(cashFlows);
+
+  setResults([`${(tir * 100).toFixed(2)}%`])
+}
+
