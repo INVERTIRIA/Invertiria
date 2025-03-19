3309a4db32e38e6366cb29ef67cf2b8378c8f3fb
kevinquique12 <kevn.1995.cod@gmail.com>
2025-03-19 11:09:14 -0500

    commit

--- invertiria/src/pages/Investment.jsx
+++ invertiria/src/pages/Investment.jsx
@@ -5,48 +5,58 @@ import { irr } from 'financial'
 // Componentes
 import { InvestForm } from "../components/InvestForm";
 import { InvestResult } from '../components/InvestResults';
+import { MyLineChart } from "../components/ChartSimple";
 
 // Pagina de inversion
 function Investment() {
 
   const { t } = useTranslation();
-
-  const [results, setResults] = useState([]);
+  const [tir, setTir] = useState([]);
 
   // Flujo de caja
-  const cashFlows = [-1000, 300, 400, 500, 200];
+  const cashFlows = [-1000, 300, 400, 500, 200, 300, 600, 500, 400, 300, 200, 100];
 
   useEffect(() => {
-    getResults(cashFlows, setResults);
+    getResults(cashFlows, setTir);
   }, []);
 
   return (
-    <>
+    <div>
       <InvestForm
         title={t("investment.title")}
         paragraph={t("investment.paragraph")}
       />
       <InvestResult
-        title={"Resultados"}
-        paragraph={"Aqui estan los resultados de tu inversion:"}
+        title={t("investment.results")}
+        paragraph={t("investment.here_are_the_results")}
         results={
           <>
             <p className="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-cyan-50">Cash Flows: {cashFlows.toString()}</p>
-            <p className="mx-auto mt-2 max-w-xl text-lg/8 text-pretty text-cyan-50">TIR: {results[0]}</p>
+            <p className="mx-auto mt-2 max-w-xl text-lg/8 text-pretty text-cyan-50">TIR: {tir.map(e => e + "%").toString()}</p>
           </>
         }
       />
-    </>
+      <MyLineChart results={cashFlows} dataName="CashFlows" />
+      <MyLineChart results={tir} dataName="TIR (%)"/>
+    </div>
   )
 }
 
 export { Investment }
 
 // FUNCIONES
-function getResults(cashFlows, setResults) {
+function getResults(cashFlows, setTir) {
 
   // Calcular la TIR
-  const tir = irr(cashFlows);
-  setResults([`${(tir * 100).toFixed(2)}%`])
+  let results = [];
+  for (let i = 0; i < cashFlows.length; i++) {
+    let tir = irr(cashFlows.slice(0, i + 1));
+    if (isNaN(tir) || tir === Infinity) {
+      results.push(0);
+      continue;
+    }
+    results.push(Number((tir*100).toFixed(2)));
+  }
+  setTir(results)
 }
 
